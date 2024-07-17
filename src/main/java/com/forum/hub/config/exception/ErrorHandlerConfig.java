be51215e44a8b080fbb700b1c5ed0938f218fde9
leo <leonardo.sampaiou@gmail.com>
2024-07-16 21:35:55 -0300

    /

---
+++ src/main/java/com/forum/hub/config/exception/ErrorHandlerConfig.java
@@ -0,0 +1,15 @@
+package com.forum.hub.config.exception;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandlerConfig {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity tratarError404(ObjectNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
---
+++ src/main/java/com/forum/hub/controller/AutenticacaoController.java
@@ -0,0 +1,16 @@
+package com.forum.hub.controller;
+import api.forum.hub.domain.dto.AutenticacaoRequest;
+import api.forum.hub.config.security.TokenService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class AutenticacaoController {
+}
